283e854b4e4b1c5eb5481f3916f7addfbac04444
andrewyk99 <andrewyk99@hotmail.com>
2022-03-10 16:11:51 -0700

    added localStorage and load function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,92 +1,75 @@
 var currentDayEl = document.querySelector("#currentDay");
 var pageContentEl = document.querySelector("body");
-var tasks = [];
+var tasks = {};
 
 // function to show current day the user opens the scheduler
 var currentDay = function(){
     currentDayEl.textContent = moment().format("dddd, MMMM Do");
 };
 
-// function to determine what hour of the day it is and colorcode each hour block accordingly
-var currHour = function(){
+// function to check the current hour and color coordinate the rows
+var currHour = function() {
     var currHour = moment().hour();
-    if (currHour < 9) { // function for before the work day
-        for (var i = 9; i < 18; i++) {
-            var textareaEl = document.querySelector("#ta" + i);
+    for (var i = 9; i < 18; i++) {
+        var textareaEl = document.querySelector("#ta" + i);
+        if (currHour < i) {
             textareaEl.className = "col-lg-9 description future"
         }
-    }
-    else if (currHour >= 9 && currHour <= 17) { // function for during and the end of the work day
-        for (var i = 9; i < 18; i++) {
-            var textareaEl = document.querySelector("#ta" + i);
-            if (currHour < i) {
-                textareaEl.className = "col-lg-9 description future"
-            }
-            if (currHour === i){
-                textareaEl.className = "col-lg-9 description present"
-            }
+        if (currHour === i) {
+            textareaEl.className = "col-lg-9 description present"
+        }
+        if (currHour > i) {
+            textareaEl.className = "col-lg-9 description past"
         }
     }
 };
 
-var loadTasks = function() {
+// set's object to save into localStorage
+var saveTasks = function(){
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+};
+
+// load function to check the localStorage and print it to the page if there are tasks saved
+var loadTasks =  function(){
     tasks = JSON.parse(localStorage.getItem("tasks"));
-
-    if (!tasks) {
-        tasks = {
-            timeID: [],
-            task: []
-        };
-    }
-}
-
-var saveTasks = function() {
-    localStorage.setItem("task", JSON.stringify(tasks));
-}
-
-var taskHandler = function(event) {
-    var targetEl = event.target;
-
-    if (targetEl.matches("#btn9")) {
-        saveTask();
+    if (!tasks){ // if there are no saved tasks, must make object empty
+        tasks = {};
     }
 
-    if (targetEl.matches("#btn10")) {
-        saveTask();
-    }
+    // prints saved task in to respective row
+    $("textarea").each(function(){
+        var rowID = $(this).closest(".row").attr("id");
+        if (tasks[rowID]){
+            $(this).text(tasks[rowID].text);
+        }
+    });
+};
 
-    if (targetEl.matches("#btn11")) {
-        saveTask();
-    }
+// function to save tasks if user clicks the save button next to the specific task they wish to save
+$("button").on("click", function(){
+    var rowID = $(this).closest(".row").attr("id"); // calls the row id of the same row which the user clicks the save button
+    var textareaID = $("#" + rowID).children("textarea").attr("id"); // calls the textarea within the same row
+    var textareaCont = $("#" + textareaID).val(); // calls the value of the textarea
+    
+    tasks[rowID] = {
+        text: textareaCont,
+        textID: textareaID
+    };
 
-    if (targetEl.matches("#btn12")) {
-        saveTask();
-    }
+    saveTasks();
+});
 
-    if (targetEl.matches("#btn13")) {
-        saveTask();
-    }
+// function to refresh the page every five minutes without the user needing to manually refresh the page
+setInterval(function(){
+    currHour();
+    currentDay();
+}, (1000 * 60) * 5);
 
-    if (targetEl.matches("#btn14")) {
-        saveTask();
-    }
-
-    if (targetEl.matches("#btn15")) {
-        saveTask();
-    }
-
-    if (targetEl.matches("#btn16")) {
-        saveTask();
-    }
-
-    if (targetEl.matches("#btn17")) {
-        saveTask();
-    }
-}
+// function to check current day
 currHour();
 
 // function to show current day at top of page
 currentDay();
 
-// eventListener calls taskHandler
-pageContentEl.addEventListener("click", taskHandler);
+// function to run load tasks
+loadTasks();
